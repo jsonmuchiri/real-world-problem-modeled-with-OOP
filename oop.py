a7950be3891d84431790d20a10f0c9275ce24479
Chirimu <jmuchiri94@gmail.com>
2017-05-09 16:21:10 +0300

    added test_describe_string and test_cost_integer

--- oop.py
+++ oop.py
@@ -5,8 +5,34 @@
 # encapsulation: dividing code into public interface and a private implementation on that interface
 
 """
-class Bird(object):
-	def fly(self):
-		print("Am flying")
-duck = Bird()
-print(duck.fly())
+class Drink(object):
+	def describe(self):
+		return "Drink"
+
+	def cost(self):
+		return 0.0
+
+class Coffee(Drink):
+	def describe(self):
+		return "It's normal coffee"
+
+	def cost(self):
+		return 20
+
+class Tea(Drink):
+	def describe(self):
+		return "It's tea"
+
+	def cost(self):
+		return 25
+
+class CoffeWithMilk(Coffee):
+	def describe(self):
+		return super(CoffeWithMilk, self).describe + ", with milk"
+
+	def cost(self):
+		return super(CoffeWithMilk, self).cost + 10
+
+	
+		
+		
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-te
+import unittest
+
+from oop import Drink
+
+class TestDrink(unittest.TestCase):
+	def test_describe_string(self):
+		self.assertEqual(type(describe('coffee')), str)
+
+	def test_cost_integer(self):
+		self.assertEqual(type(cost(23)), int)
