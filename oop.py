991fafbeab558fa1fb1c1004c4b857baed003838
Chirimu <jmuchiri94@gmail.com>
2017-05-09 15:42:25 +0300

    added the oop.py file

---
+++ oop.py
@@ -0,0 +1,12 @@
+"""
+
+# inheritance: The ability to create subclasses that mimic parent class in terms of data and behaviors
+# polymorphism: Ability to overload standard operators so that they have appropriate behaviors based on their context
+# encapsulation: dividing code into public interface and a private implementation on that interface
+
+"""
+class Bird(object):
+	def fly(self):
+		print("Am flying")
+duck = Bird()
+print(duck.fly())
